59a26f76b1ac350c8d9f362375544d92f1bef490
Pedro <p.o.maxado@gmail.com>
2014-09-10 15:52:20 -0300

    Request class

--- ChatServer/src/chatserver/business/Request.java
+++ ChatServer/src/chatserver/business/Request.java
@@ -15,15 +15,28 @@ public class Request {
     private Client client;
     private Message msg;
     
+    /**
+     * This class implements a simple request from a client.
+     * @param c <code>Client</code> who sent the message.
+     * @param m <code>Message</code> sent by the <code>Client</code>.
+     */
     public Request (Client c, Message m) {
         this.client = c;
         this.msg = m;
     }
 
+    /**
+     * Gets the client who made the request.
+     * @return The <code>Client</code>.
+     */
     public Client getClient() {
         return client;
     }
 
+    /**
+     * Gets the message from sent by the client.
+     * @return The <code>Message</code> sent.
+     */
     public Message getMsg() {
         return msg;
     }
