1e505556b74972b9dc3e5355d66be8887a70a7b3
Pedro <p.o.maxado@gmail.com>
2014-10-20 14:22:32 -0200

    SignUpDAO

--- NotStarCraft/src/notstarcraft/dataTile/SignUpDAO.java
+++ NotStarCraft/src/notstarcraft/dataTile/SignUpDAO.java
@@ -7,6 +7,10 @@
 package notstarcraft.dataTile;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * 
@@ -14,6 +18,46 @@ import java.sql.Connection;
  */
 public class SignUpDAO {
 
-    private final Connection connection = ConnectionFactory.getConnection();
+    private static final Connection connection = ConnectionFactory.getConnection();
+    
+    public static boolean isUserAvailable(String username) throws SQLException {
+        
+        String sql = "SELECT * FROM users WHERE username=?";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        
+        stmt.setString(1, username);
+        
+        ResultSet rs = stmt.executeQuery();
+        
+        return rs.next();
+        
+    }
+    
+    public static void addUser(String username, String password) throws SQLException {
+        
+        String sql = "INSERT INTO users VALUES (?,?)";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        
+        stmt.setString(1, username);
+        stmt.setString(2, password);
+        
+        stmt.execute();
+        
+    }
+    
+    public static void addUserInfo(String username, String name, String gender, String email, Date birthDate) throws SQLException {
+        
+        String sql = "INSERT INTO UserInfo VALUES(?,?,?,?,?)";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        
+        stmt.setString(1, username);
+        stmt.setString(2, name);
+        stmt.setDate(3, birthDate);
+        stmt.setString(4, email);
+        stmt.setString(5, gender);
+        
+        stmt.execute();
+        
+    }
     
 }
