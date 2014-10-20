8fe659c49a7fafc5ce54e31a5493c83dbb591112
Pedro <p.o.maxado@gmail.com>
2014-10-20 14:22:18 -0200

    SignInDAO

--- NotStarCraft/src/notstarcraft/dataTile/SignInDAO.java
+++ NotStarCraft/src/notstarcraft/dataTile/SignInDAO.java
@@ -7,6 +7,10 @@
 package notstarcraft.dataTile;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import notstarcraft.game.player.Player;
 
 /**
  * 
@@ -14,6 +18,50 @@ import java.sql.Connection;
  */
 public class SignInDAO {
     
-    private final Connection connection = ConnectionFactory.getConnection();
+    private static final Connection connection = ConnectionFactory.getConnection();
+    
+    public static boolean isUserValid(String username, String password) throws SQLException {
+        
+        String sql = "SELECT * FROM users WHERE username=?";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        
+        stmt.setString(1, username);
+        
+        ResultSet rs = stmt.executeQuery();
+        
+//        if( rs.next() )
+//            if( rs.getString("password").equals(password) )
+//                return true;
+        
+        return rs.next() && rs.getString("password").equals(password);
+        
+    }
+    
+    public static Player getPlayer(String username) throws SQLException {
+        
+        String sql = "SELECT * FROM UserInfo WHERE username=?";
+        PreparedStatement stmt = connection.prepareCall(sql);
+        
+        stmt.setString(1, username);
+        
+        ResultSet rs = stmt.executeQuery();
+        
+        if( rs.next() ) {
+            
+            Player player = new Player();
+            
+            player.setName(rs.getString("name"));
+            player.setNickname(rs.getString("username"));
+            player.setEmail(rs.getString("email"));
+            player.setGender(rs.getString("gender"));
+            player.setBirthDate(rs.getDate("birthDate"));
+            
+            return player;
+            
+        }
+        
+        return null;
+        
+    }
     
 }
