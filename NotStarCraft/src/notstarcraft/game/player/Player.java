d04a200cb24bbd4bd44d222b44d30bf5def9c70a
Pedro <p.o.maxado@gmail.com>
2014-10-20 14:23:19 -0200

    Player

---
+++ NotStarCraft/src/notstarcraft/game/player/Player.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package notstarcraft.game.player;
+
+import java.sql.Date;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Player {
+    
+    private String name;
+    private String nickname;
+    private String email;
+    private Date birthDate;
+    private String gender;
+    
+    public Player() { }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+}
